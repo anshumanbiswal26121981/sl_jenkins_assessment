163ea9107b0055f778f33b38420e758532e91521
abiswal <abiswal@vmware.com>
2020-11-24 08:51:40 +0530

    first commit

---
+++ src/main/java/com/simplilearn/sl_jenkins_assessment/sl_jenkins_assessment/SlJenkinsAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.sl_jenkins_assessment.sl_jenkins_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlJenkinsAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlJenkinsAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/sl_jenkins_assessment/sl_jenkins_assessment/SlJenkinsAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.sl_jenkins_assessment.sl_jenkins_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlJenkinsAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
