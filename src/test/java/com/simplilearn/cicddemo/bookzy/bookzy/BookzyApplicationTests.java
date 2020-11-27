d7772f28c80c812f275490e2bd23dbc130d0d376
abiswal <abiswal@vmware.com>
2020-11-27 15:14:56 +0530

    new commit

---
+++ src/test/java/com/simplilearn/cicddemo/bookzy/bookzy/BookzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.cicddemo.bookzy.bookzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
